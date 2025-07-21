a45040facb41d52dae7d891cc506143d22428620
kmart064 <ktm064@gmail.com>
2025-07-22 03:44:44 +0900

    fix: support environment variables in production (Render)

--- server/src/preload.ts
+++ server/src/preload.ts
@@ -1,4 +1,5 @@
 import * as dotenv from "dotenv";
 
-const env = process.env.NODE_ENV || "development";
-dotenv.config({ path: `.env.${env}` });
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config({ path: `.env.${process.env.NODE_ENV || "development"}` });
+}
