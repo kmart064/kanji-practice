548f1dcb55a2828a7fe1df2bc01efe44cdfb2889
kmart064 <ktm064@gmail.com>
2025-08-16 00:29:31 +0900

    feat: changed access token lifespan to 1 day

--- server/src/services/tokenService.ts
+++ server/src/services/tokenService.ts
@@ -4,7 +4,7 @@ const ACCESS_SECRET = process.env.ACCESS_SECRET!;
 const REFRESH_SECRET = process.env.REFRESH_SECRET!;
 
 export function generateAccessToken(user: { id: string; username: string }) {
-  return jwt.sign(user, ACCESS_SECRET, { expiresIn: "15m" });
+  return jwt.sign(user, ACCESS_SECRET, { expiresIn: "1d" });
 }
 
 export function generateRefreshToken(user: { id: string; username: string }) {
