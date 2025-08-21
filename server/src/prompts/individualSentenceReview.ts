6a99e00d724efea6f4108fec2192f122e16035e4
kmart064 <ktm064@gmail.com>
2025-08-22 01:15:50 +0900

    feat: changed review type to be configurable

---
+++ server/src/prompts/individualSentenceReview.ts
@@ -0,0 +1,11 @@
+export const getSentenceReview = (kanjiList: string) =>
+  `
+For the following words, please generate one sentence using each word. 
+Do not put furigana as the purpose is to test that I can read and 
+understand the kanji, as well as see how it can be used. Other kanji 
+can be used, but try to keep the other words N4 or below.
+You may use any **grammatical form** of the kanji (e.g., 眠る → 眠った、眠れない, etc.) 
+including transitive/intransitive forms
+
+Words: "${kanjiList}"
+`;
--- server/src/services/getReadingPassage.ts
+++ server/src/services/getReadingPassage.ts
@@ -1,8 +1,14 @@
+import { env } from "process";
 import { ReviewEntry } from "../models/ReviewEntry.js";
 import { Word } from "../models/Word.js";
+import { getSentenceReview } from "../prompts/individualSentenceReview.js";
 import { getThemedPassage } from "../prompts/themedReview.js";
+import { getFlexiblePassage } from "../prompts/basicReview.js";
 
 export function getFullPrompt(kanjiList: Word[] | ReviewEntry[]): string {
   const kanji = kanjiList.map((k) => k.kanji).join(", ");
-  return getThemedPassage(kanji);
+  const reviewType = process.env.REVIEW_TYPE;
+  if (reviewType === "sentence") return getSentenceReview(kanji);
+  else if (reviewType === "theme") return getThemedPassage(kanji);
+  else return getFlexiblePassage(kanji);
 }
