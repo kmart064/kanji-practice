dbcbf5f6b735476b4521ebe1f0ba0726709a483a
kmart064 <ktm064@gmail.com>
2025-12-19 14:45:39 +0900

    feat: changed request prompt for AI

--- server/src/prompts/individualSentenceReview.ts
+++ server/src/prompts/individualSentenceReview.ts
@@ -3,9 +3,10 @@ export const getSentenceReview = (kanjiList: string) =>
 For the following words, please generate one sentence using each word. 
 Do not put furigana as the purpose is to test that I can read and 
 understand the kanji, as well as see how it can be used. Other kanji 
-can be used, but try to keep the other words N4 or below.
+can be used, but try to keep the other words N3 or below.
 You may use any **grammatical form** of the kanji (e.g., 眠る → 眠った、眠れない, etc.) 
-including transitive/intransitive forms
+including transitive/intransitive/passive/causative forms
+Please do not bold or otherwise show the word used in each sentence.
 
 Words: "${kanjiList}"
 `;
